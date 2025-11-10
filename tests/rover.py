14d5128d6b73e972d9297453dc2ba689304c692a
WMOkiishi <w.muneo.o@gmail.com>
2025-11-10 08:53:55 -0700

    Move things around in rover.py

--- tests/rover.py
+++ tests/rover.py
@@ -11,11 +11,6 @@ from rift.robot import RobotInverse
 from rift.state import RobotState
 from rift.truss_config import CONFIG_3D_ROVER1 as config
 
-path = make_path(xform=roll_pitch_yaw(np.pi/4, np.pi/6, np.pi/4))
-path += config.initial_pos[config.move_node]
-robot = RobotInverse(config)
-plot = RoverPlotter3D(config)
-
 
 def initialize_fig(plotter: RoverPlotter3D, state: RobotState, path: Matrix) -> go.Figure:
     return go.Figure(
@@ -54,13 +49,19 @@ def initialize_fig(plotter: RoverPlotter3D, state: RobotState, path: Matrix) -> 
     )
 
 
-fig = initialize_fig(plot, robot.state, path)
-frames = [go.Frame(data=plot.generate_data(robot.state, path))]
+def main():
+    path = make_path(xform=roll_pitch_yaw(np.pi/4, np.pi/6, np.pi/4))
+    path += config.initial_pos[config.move_node]
+    robot = RobotInverse(config)
+    plot = RoverPlotter3D(config)
 
-for pos, vel in robot.move_node_along_path(config.move_node, path):
-    frames.append(go.Frame(data=plot.generate_data(robot.state, path)))
+    fig = initialize_fig(plot, robot.state, path)
+    frames = [go.Frame(data=plot.generate_data(robot.state, path))]
+    for pos, vel in robot.move_node_along_path(config.move_node, path):
+        frames.append(go.Frame(data=plot.generate_data(robot.state, path)))
+    fig.frames = frames
+    fig.show()
 
-fig.frames = frames
 
 if __name__ == '__main__':
-    fig.show()
+    main()
