456957cd2aea8dc401750161959befe40fd83f34
WMOkiishi <w.muneo.o@gmail.com>
2025-11-21 23:22:00 -0700

    Add a script for profiling

---
+++ tests/prof.py
@@ -0,0 +1,26 @@
+import pathlib, sys
+sys.path.append(str(pathlib.Path.cwd()))
+
+import cProfile
+import pstats
+import time
+
+from rift.gentools import expend
+from rift.robot import RobotInverse
+from rift.truss_config import CONFIG_ROVER as config
+
+
+def main():
+    t0 = time.time()
+    with cProfile.Profile() as profile:
+        robot = RobotInverse(config)
+        for _ in range(10):
+            expend(robot.crawl())
+    t1 = time.time()
+    stats = pstats.Stats(profile).sort_stats(pstats.SortKey.CUMULATIVE)
+    stats.print_stats(8)
+    print(f"Total time (imprecise): {t1 - t0:.2g}")
+
+
+if __name__ == '__main__':
+    main()
