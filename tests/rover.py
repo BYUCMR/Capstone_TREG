a786cce6fdd08fb5a4df001361355a0149d34895
WMOkiishi <w.muneo.o@gmail.com>
2025-11-25 10:02:03 -0700

    Modify rover.py

--- tests/rover.py
+++ tests/rover.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import rift.anim
 from rift.robot import InverseKinematicsError, RobotInverse
 from rift.state import RobotState
-from rift.truss_config import CONFIG_ROVER as config, TrussConfig
+from rift.truss_config import TrussConfig
 
 
 def initialize_fig(config: TrussConfig, state: RobotState) -> go.Figure:
@@ -51,19 +51,27 @@ def initialize_fig(config: TrussConfig, state: RobotState) -> go.Figure:
     )
 
 
-def main():
+def main(
+    config: TrussConfig,
+    *,
+    step_length: float = 0.8,
+    cycles: int = 1,
+    resolution: int = 50,
+):
     robot = RobotInverse(config)
     fig = initialize_fig(config, robot.state)
     frames: list[go.Frame] = []
     try:
-        for path in robot.crawl():
-            data = rift.anim.generate_data(config, robot.state, path)
-            frames.append(go.Frame(data=data))
-    except InverseKinematicsError:
-        pass
+        for _ in range(cycles):
+            for path in robot.crawl(step_length, resolution=resolution):
+                data = rift.anim.generate_data(config, robot.state, path)
+                frames.append(go.Frame(data=data))
+    except InverseKinematicsError as e:
+        print(e.args[0])
     fig.frames = frames
     fig.show()
 
 
 if __name__ == '__main__':
-    main()
+    from rift.truss_config import CONFIG_ROVER as config
+    main(config, cycles=4, resolution=100)
