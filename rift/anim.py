c42f4eae483b830e14e35caf24fc05b5ad11a4eb
WMOkiishi <w.muneo.o@gmail.com>
2025-12-06 15:17:18 -0700

    Replace Plotly with PyQtGraph
    PyQtGraph is in general better suited to our use case.
    
    I have elected to use a coroutine-based event loop,
    since that usually makes for cleaner code than callbacks.

--- rift/anim.py
+++ rift/anim.py
@@ -1,8 +1,10 @@
+import asyncio
 from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Protocol, Self
 
-import plotly.graph_objects as go
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
 
 from . import tubetruss as tt
 from .truss_config import TrussConfig
@@ -10,46 +12,49 @@ from .typing import Matrix
 
 
 class AnimationItem(Protocol):
-    def add_to_frame(self, data: list, /) -> None: ...
+    def add_to_view(self, view: gl.GLViewWidget, /) -> None: ...
     def update_pos(self, pos: Matrix, /) -> None: ...
 
 
 @dataclass(slots=True, frozen=True)
 class DrawnTube(AnimationItem):
     tube: tt.Tube
-    drawing: go.Scatter3d
+    drawing: gl.GLLinePlotItem
 
-    def add_to_frame(self, data: list) -> None:
-        data.append(self.drawing)
+    def add_to_view(self, view: gl.GLViewWidget) -> None:
+        view.addItem(self.drawing)
 
     def update_pos(self, pos: Matrix) -> None:
-        x, y, z = pos[list(self.tube.nodes)].T
-        self.drawing.update(x=x, y=y, z=z)
+        self.drawing.setData(pos=pos[list(self.tube.nodes)])
 
 
 @dataclass(slots=True, frozen=True)
 class PayloadMesh(AnimationItem):
     payload: tt.TubeTruss
-    mesh: go.Mesh3d
+    mesh: gl.GLMeshItem
 
-    def add_to_frame(self, data: list) -> None:
-        data.append(self.mesh)
+    def add_to_view(self, view: gl.GLViewWidget) -> None:
+        view.addItem(self.mesh)
 
     def update_pos(self, pos: Matrix) -> None:
         nodes = set[tt.Node]()
         nodes.update(*(bar.nodes for bar in self.payload))
-        x, y, z = pos[sorted(nodes)].T
-        self.mesh.update(x=x, y=y, z=z)
+        # `GLMeshItem.opts` might be an implementation detail, but this is the
+        # cleanest way to update vertex positions while keeping everything
+        # else the same.
+        mesh_data: gl.MeshData = self.mesh.opts['meshdata']
+        mesh_data.setVertexes(pos[list(nodes)])
+        self.mesh.setMeshData(meshdata=mesh_data)
 
 
 def draw_tube(tube: tt.Tube, pos: Matrix, *, color: str = 'gray', width: int = 6) -> DrawnTube:
-    x, y, z = pos[list(tube.nodes)].T
-    drawing = go.Scatter3d(
-        x=x, y=y, z=z,
-        mode='lines',
-        line=dict(width=width, color=color),
-        showlegend=False,
+    tube_vertices = pos[list(tube.nodes)]
+    drawing = gl.GLLinePlotItem(
+        pos=tube_vertices,
+        width=width,
+        color=pg.mkColor(color),
     )
+    drawing.setGLOptions('opaque')
     return DrawnTube(tube, drawing)
 
 
@@ -61,22 +66,21 @@ def draw_payload_mesh(payload: tt.TubeTruss, pos: Matrix) -> PayloadMesh:
     payload_nodes = set[tt.Node]()
     payload_nodes.update(*(bar.nodes for bar in payload))
 
-    x, y, z = pos[sorted(payload_nodes)].T
+    payload_vertices = pos[sorted(payload_nodes)]
     payload_faces = [[0, 1, 2], [3, 4, 5],
                      [0, 3, 5], [0, 2, 5],
                      [1, 4, 5], [1, 2, 5],
                      [0, 1, 4], [0, 3, 4]]
 
-    mesh = go.Mesh3d(
-        x=x, y=y, z=z,
-        i=[f[0] for f in payload_faces],
-        j=[f[1] for f in payload_faces],
-        k=[f[2] for f in payload_faces],
-        color='lightblue',
-        opacity=1,
-        flatshading=True,
-        name='Prism'
+    meshdata = gl.MeshData(
+        vertexes=payload_vertices,
+        faces=payload_faces,
     )
+    mesh = gl.GLMeshItem(
+        meshdata=meshdata,
+        color=pg.mkColor('lightblue'),
+    )
+    mesh.setGLOptions('opaque')
     return PayloadMesh(payload, mesh)
 
 
@@ -97,72 +101,32 @@ def draw_items(config: TrussConfig, pos: Matrix) -> list[AnimationItem]:
     return [payload_mesh, *payload_bars, *triangles]
 
 
-def initialize_fig(initial_data) -> go.Figure:
-    return go.Figure(
-        data=initial_data,
-        layout=go.Layout(
-            updatemenus=[dict(
-                type='buttons',
-                buttons=[dict(
-                    label='Play',
-                    method='animate',
-                    args=[None, dict(
-                        frame=dict(duration=10, redraw=True),
-                        transition=dict(duration=0),
-                    )]
-                )]
-            )],
-            scene=dict(
-                aspectmode='cube',
-                bgcolor='white',
-                xaxis=dict(
-                    range=(-8, 12),
-                    backgroundcolor='white',
-                    gridcolor='lightgray',
-                    zerolinecolor='lightgray',
-                ),
-                yaxis=dict(
-                    range=(-10, 10),
-                    backgroundcolor='white',
-                    gridcolor='lightgray',
-                    zerolinecolor='lightgray',
-                ),
-                zaxis=dict(
-                    range=(-1, 19),
-                    backgroundcolor='white',
-                    gridcolor='lightgray',
-                    zerolinecolor='lightgray',
-                )
-            )
-        )
-    )
-
-
 @dataclass(slots=True, frozen=True)
 class Animator:
+    view: gl.GLViewWidget
     items: Iterable[AnimationItem] = ()
 
     @classmethod
     def from_config(cls, config: TrussConfig) -> Self:
+        pg.mkQApp()
+        view = gl.GLViewWidget()
+        view.addItem(gl.GLGridItem())
         items = draw_items(config, config.initial_pos)
-        return cls(items)
+        for item in items:
+            item.add_to_view(view)
+        return cls(view, items)
 
     def update_pos(self, pos: Matrix) -> None:
         for item in self.items:
             item.update_pos(pos)
 
-    def make_frame(self) -> go.Frame:
-        data = []
-        for item in self.items:
-            item.add_to_frame(data)
-        return go.Frame(data=data)
-
-    def animate(self, positions: Iterable[Matrix] = ()) -> None:
-        frames: list[go.Frame] = []
-        for pos in positions:
+    async def animate(self, positions: asyncio.Queue[Matrix]) -> None:
+        self.view.show()
+        while True:
+            try:
+                pos = await positions.get()
+            except asyncio.QueueShutDown:
+                break
             self.update_pos(pos)
-            frame = self.make_frame()
-            frames.append(frame)
-        fig = initialize_fig(frames[0].data)
-        fig.frames = frames
-        fig.show()
+            positions.task_done()
+            await asyncio.sleep(0.01)
--- tests/animate.py
+++ tests/animate.py
@@ -1,13 +1,18 @@
 import pathlib, sys
 sys.path.append(str(pathlib.Path.cwd()))
 
+import asyncio
+
+import pyqtgraph
+from PySide6 import QtAsyncio
+
 import rift.anim
 from rift.robot import InverseKinematicsError, RobotInverse
 from rift.truss_config import TrussConfig
 from rift.typing import Matrix
 
 
-def main(
+async def main(
     config: TrussConfig,
     *,
     step_length: float = 0.8,
@@ -16,15 +21,25 @@ def main(
 ) -> None:
     animator = rift.anim.Animator.from_config(config)
     robot = RobotInverse.from_config(config)
-    positions: list[Matrix] = []
-    try:
+    positions = asyncio.Queue[Matrix](resolution)
+
+    async def crawl() -> None:
         for _ in robot.crawl(cycles, step_length, resolution=resolution):
-            positions.append(robot.pos.copy())
+            await positions.put(robot.pos.copy())
+
+    crawling_task = asyncio.create_task(crawl())
+    animation_task = asyncio.create_task(animator.animate(positions))
+    try:
+        await crawling_task
     except InverseKinematicsError as e:
         print(e.args[0])
-    animator.animate(positions)
+    print("Done with IK")
+    positions.shutdown()
+    await animation_task
+    print("Done with animation")
 
 
 if __name__ == '__main__':
     from rift.truss_config import CONFIG_ROVER as config
-    main(config, cycles=4, resolution=100)
+    pyqtgraph.mkQApp()
+    QtAsyncio.run(main(config, cycles=4, resolution=100))
