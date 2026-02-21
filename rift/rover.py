b6520b1a7f8a1f9370a57b597eced0ed35d14c22
WMOkiishi <w.muneo.o@gmail.com>
2026-02-20 22:31:44 -0700

    Implement rolling motion

--- rift/rover.py
+++ rift/rover.py
@@ -137,7 +137,50 @@ def make_pos(
     return pos
 
 
+def make_pos_sitting(
+    payload_length: float,
+    payload_angle: float,
+    payload_width: float,
+    foot_distance: float,
+    *,
+    tube_length: float = 3.
+) -> Matrix:
+    # Perimter of triangle on payload
+    # angle of payload size
+    # width of payload
+    # width of feet
+
+    payload_angle = math.radians(payload_angle)
+    y0 = payload_width * 0.5
+    pos = np.zeros((12, 3))
+
+    # Feet
+    fx = foot_distance * 0.5
+    fy = math.sqrt(0.25*tube_length**2 - tube_length*fx)
+    ez = fx * math.sqrt(0.75*tube_length**2 + tube_length*fx) / (0.5*tube_length + fx)
+    pos[L1] = [ fx,  y0+fy, 0.]
+    pos[L2] = [-fx,  y0+fy, 0.]
+    pos[R1] = [ fx, -y0-fy, 0.]
+    pos[R2] = [-fx, -y0-fy, 0.]
+    pos[L3] = [ 0.,  y0+fy, ez]
+    pos[R3] = [ 0., -y0-fy, ez]
+
+    # Payload
+    px = payload_length * 0.5
+    py = px * math.sqrt(3.) * math.sin(payload_angle)
+    pz = px * math.sqrt(3.) * math.cos(payload_angle)
+    pos[PL1] = [-px,  y0+py, pz]
+    pos[PL2] = [ px,  y0+py, pz]
+    pos[PR1] = [-px, -y0-py, pz]
+    pos[PR2] = [ px, -y0-py, pz]
+    pos[PL3] = [0.,  y0, 0.]
+    pos[PR3] = [0., -y0, 0.]
+
+    return pos
+
+
 CRAWLING_POS: Final = make_pos(0.625, 0.5, 0, 1.25, 0.875)
+ROLLING_POS: Final = make_pos(0, 0.5, 0, 1.25, 0.875)
 
 
 def make_robot(init_pos: Matrix = CRAWLING_POS) -> RobotInverse:
@@ -190,7 +233,7 @@ def make_animator(init_pos: Matrix = CRAWLING_POS) -> anim.Animator:
     payload_mesh = draw_payload_mesh(PAYLOAD_STRUCTURE, init_pos)
     payload_bars = draw_payload_bars(PAYLOAD_STRUCTURE, init_pos)
     triangles = draw_triangles(LEG_STRUCTURE, init_pos)
-    traces = anim.draw_traces((L1, L2, R1, R2), init_pos)
+    traces = anim.draw_traces(range(12), init_pos)
     items = [payload_mesh, payload_bars, *triangles, *traces]
     for item in items:
         item.add_to_view(view)
@@ -224,3 +267,67 @@ def crawl(
             no_wobble,
         ])
         yield from robot.take_step(motion, resolution=resolution)
+
+
+def roll(
+    robot: RobotInverse,
+    *,
+    i: int = 0,
+    resolution: int = 100,
+) -> Generator[tuple[Matrix, Vector]]:
+    payload_midpoints = (
+        cstr.Point.avg(CPL1, CPR1),
+        cstr.Point.avg(CPL3, CPR3),
+        cstr.Point.avg(CPL2, CPR2),
+    )
+    foot_pairs = (
+        (CL1, CR1),
+        (CL3, CR3),
+        (CL2, CR2),
+    )
+    base = payload_midpoints[i-2]
+    face = payload_midpoints[i-1]
+    foot_l, foot_r = foot_pairs[i]
+    arm_l, arm_r = foot_pairs[i-2]
+    other_feet = [
+        foot
+        for j, pair in enumerate(foot_pairs)
+        for foot in pair
+        if j != i
+    ]
+    payload_mass = np.zeros(len(robot.pos))
+    payload_mass[PAYLOAD] = 1.
+    payload_com = cstr.Point.com(payload_mass)
+    feet_midpoint = cstr.Point.avg(foot_l, foot_r)
+    step_1 = cstr.CompoundConstraint((
+        cstr.Motion.lock(base),
+        cstr.Orbit.about_y(robot.pos, face-base, np.pi, resolution),
+        cstr.Motion.make(face - base, y=0.),
+        cstr.Motion.lock(foot_l),
+        cstr.Motion.lock(foot_r),
+        *(
+            cstr.Motion.make(foot, y=0.)
+            for foot in other_feet
+        ),
+    ))
+    yield from robot.take_step(step_1, resolution=resolution, allow_redundant=True)
+    dx = ((face - feet_midpoint).get(robot.pos)[0] - 0.5*0.875) / resolution
+    foot_arc = partial(steps.parabolic, -dx)
+    step_2 = cstr.CompoundConstraint((
+        cstr.Motion.lock(payload_com),
+        cstr.Motion.make(face - base, z=0.),
+        cstr.Motion.make(foot_l, x=dx, z=foot_arc),
+        cstr.Motion.make(foot_r, x=dx, z=foot_arc),
+    ))
+    yield from robot.take_step(step_2, resolution=resolution)
+    step_3 = cstr.CompoundConstraint((
+        cstr.Motion.lock(face),
+        cstr.Orbit.about_y(robot.pos, base-face, np.pi/3, resolution),
+        cstr.Motion.make(payload_com - face, y=0.),
+        cstr.Motion.make(base - face, y=0.),
+        cstr.Motion.lock(foot_l),
+        cstr.Motion.lock(foot_r),
+        cstr.Orbit.about_y(robot.pos, arm_l-foot_l, np.pi, resolution),
+        cstr.Orbit.about_y(robot.pos, arm_r-foot_r, np.pi, resolution),
+    ))
+    yield from robot.take_step(step_3, resolution=resolution)
