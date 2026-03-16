db4eb22e7f7659a2527730b1d5f2d2c259ec7996
WMOkiishi <w.muneo.o@gmail.com>
2026-03-16 08:35:14 -0600

    Implement new control system

--- rift/ui/Handlers/transmit.py
+++ rift/ui/Handlers/transmit.py
@@ -1,17 +1,21 @@
 import time
 
+import numpy as np
 import serial
 from numpy import ndarray
 from PySide6.QtCore import QObject, QThread, Signal, Slot
 
 from rift import rover
 from rift.arraytypes import Matrix, Vector
-from rift.transmit import commands
+from rift.transmit import commander, commands
 
 
 class TransmitHandler(QObject):
-    direct = Signal(bytes)
     message = Signal(str)
+    update_sliders = Signal(np.ndarray)
+    set_zero = Signal()
+    to_zero = Signal()
+    catch_up = Signal()
     bot_live = False
 
     def start_transmission(self, *, port: str) -> None:
@@ -20,7 +24,10 @@ class TransmitHandler(QObject):
         self.worker.moveToThread(self.work_thread)
 
         self.worker.message.connect(self.message.emit)
-        self.direct.connect(self.worker.transmit_direct)
+        self.worker.update_sliders.connect(self.update_sliders.emit)
+        self.set_zero.connect(self.worker.set_zero)
+        self.to_zero.connect(self.worker.to_zero)
+        self.catch_up.connect(self.worker.catch_up)
         self.work_thread.finished.connect(self.worker.deleteLater)
         self.worker.start(port)
         self.work_thread.start()
@@ -35,32 +42,54 @@ class TransmitHandler(QObject):
 
 class TransmitWorker(QObject):
     message = Signal(str)
+    update_sliders = Signal(np.ndarray)
 
-    def __init__(self, *, timeout: float = 2.5, parent: QObject | None = None) -> None:
+    def __init__(self, *, timeout: float = 0.01, parent: QObject | None = None) -> None:
         super().__init__(parent)
         self.ser = serial.Serial(baudrate=115200, timeout=timeout)
+        self.commander = commander.Commander(
+            ser=self.ser,
+            q_des=np.zeros(12, dtype=np.intp),
+            log=self.message.emit,
+        )
 
-    @Slot(bytes)
-    def transmit_direct(self, cmd: bytes) -> None:
+    @Slot()
+    def set_zero(self) -> None:
         if not self.ser.is_open:
             return
-        self.ser.write(cmd)
-        self.ser.flush()
+        self.commander.set_zero()
+
+    @Slot()
+    def to_zero(self) -> None:
+        if not self.ser.is_open:
+            return
+        self.commander.to_zero()
+        time.sleep(3.0)
+        error = self.commander.get_error()
+        if error is not None:
+            self.update_sliders.emit(error)
+
+    @Slot()
+    def catch_up(self) -> None:
+        self.commander.catch_up()
+        time.sleep(3.0)
+        error = self.commander.get_error()
+        if error is not None:
+            self.update_sliders.emit(error)
 
     @Slot(ndarray, ndarray)
     def transmit(self, x: Matrix, dq: Vector) -> None:
         if not self.ser.is_open:
             return
-        ticks = rover.TICKS_PER_SIDE * dq
+        error = self.commander.get_error()
+        if error is not None:
+            self.update_sliders.emit(error)
+        ticks = (rover.TICKS_PER_SIDE * dq).astype(np.intp)
+        self.commander.update(ticks)
+        if error is not None:
+            ticks += error
         dt = commands.get_smallest_dt(ticks, max_speed=1000)
-        cmd = commands.VEL(ticks/dt, dt)
-        self.ser.writelines((commands.STOP, cmd))
-        self.ser.flush()
-        if responses := self.ser.read_all():
-            for response in responses.splitlines():
-                if not response.startswith(b'['):
-                    self.message.emit(response.decode())
-        time.sleep(dt)
+        self.commander.send_dq(ticks, dt)
 
     def start(self, port: str) -> None:
         self.ser.port = port
